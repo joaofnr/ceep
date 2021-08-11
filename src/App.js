7a19fd1e39d8bc2db422b4e7b233c804cbe8bafb
João Nunes Rios <joao.rios@compasso.com.br>
2021-08-10 21:04:15 -0300

    Project created with first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ListaDeNotas from './components/ListaDeNotas';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input type="text" placeholder="Título" />
+        <br />
+        <textarea placeholder="Escreva sua nota" />
+        <br />
+        <button>Criar nota</button>
+      </form>
+      <ListaDeNotas />
+    </>
   );
 }
-
-export default App;
