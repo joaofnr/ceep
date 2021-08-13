4ceac02c0b5e1802c4ffdb771efe5c3026439f9f
João Nunes Rios <joao@pw3.com.br>
2021-08-13 07:17:42 -0300

    Populating array of notes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import './assets/App.css'
 
 export default class App extends Component {
 
-  // função para criar a nota que poder ser
+  constructor(){
+    super()
+    this.notas = []
+  }
+
+  // função para criar a nota será
   // chamada internamente em algum componente
+  // cria um novo objeto com o título e o texto
+  // e popula o array de notas desta classe
   criarNota(titulo, texto){
-    console.log(`Criar nota -  ${titulo}: ${texto}`)
+    const novaNota = {titulo, texto}
+    this.notas.push(novaNota)
   }
 
   // tudo que estiver dentro do render() será
@@ -20,11 +28,19 @@ export default class App extends Component {
   // Ao definir um componente, podemos passar propriedades
   // que quisermos para o mesmo.
   // Isso é uma injeção de dependência
+
+  // No primeiro elemento, passamos o método criarNota que será 
+  // chamado pelo formulário. No segundo, passamos a propriedade,
+  // que será uma array populada na função criarNota
+
+  // Importante entender que, para a propriedade criarNota fazer a 
+  // interação com o componente filho e trazer de volta um valor
+  // que será usado neste componente, é preciso usar o bind(this)
   render () { 
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.notas} />
       </section>
     )
   };
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -15,13 +15,12 @@ export default class ListaDeNotas extends Component{
         return (
             <ul 
               className="lista-notas">
-              {Array.of("Trabalho", "Casa", "Estudos").map((categoria, index) => {
+              {this.props.notas.map((nota, index) => {
                   return(
                     <li 
                       key={index}
                       className="lista-notas_item"
                       >
-                      <div>{categoria}</div>
                       <CardNota />
                     </li>
                   )
